762856cc8a0d2b31ca706ee49b66fdde4e74d59d
smallMote <1746671849@qq.com>
2020-03-24 00:26:01 +0800

    命令链接

--- pack.js
+++ pack.js
@@ -2,7 +2,7 @@
 // console.log('pack is start!')
 const path = require('path');
 // 1.拿到配置文件
-const config = require(path.resolve(__dirname));
+const config = require(path.resolve('webpack.config.js'));
 // 2.解析配置文件
 const Compiler = require('./lib/Compiler.js');
 const compiler = new Compiler(config);

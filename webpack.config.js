003d63145488412e9c62b9bc882eefcb2dd733c1
smallMote <1746671849@qq.com>
2020-03-25 09:21:39 +0800

    配置loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename : 'bundle.pack.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [ // 加载自己编写的loader
+          path.resolve(__dirname, 'loader', 'style-loader'),
+          path.resolve(__dirname, 'loader', 'less-loader')
+        ]
+      }
+    ]
   }
 }
